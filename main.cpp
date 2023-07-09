5205933eef0b4ee7922a1469b1c671b552331ab5
Shine Chang <shine00chang@gmail.com>
2023-07-09 00:39:27 -0700

    (impl) renderer

--- main.cpp
+++ main.cpp
@@ -11,14 +11,14 @@ int main( int argc, char* args[] )
 	//The window we'll be rendering to
 	SDL_Window* window = NULL;
 	
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+	// Setup renderer
+    SDL_Renderer* renderer = NULL;
 
 	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) 
+    {
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-
+    }
 	else
 	{
 		//Create window
@@ -27,20 +27,45 @@ int main( int argc, char* args[] )
 			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
 		else
 		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface( window );
+            // Setup renderer 
+            renderer =  SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
 
-			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
+            constexpr int a = 1;
+            constexpr int t = 1;
+            /*
+            // Render moving block
+            for (int t=0; t<500; t++) 
+            {
+            */
+                // Set render color to red, then clear ( background will be rendered in this color )
+                SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
+                SDL_RenderClear( renderer );
+
+                // Creat a rect at pos ( 50, 50 ) that's 50 pixels wide and 50 pixels high.
+                SDL_Rect r;
+                r.x = 2 * a * t;
+                r.y = 2 * a * t * t;
+                r.w = 50;
+                r.h = 50;
+
+                // Set render color to blue ( rect will be rendered in this color )
+                SDL_SetRenderDrawColor( renderer, 0, 0, 255, 255 );
+
+                // Render rect
+                SDL_RenderFillRect( renderer, &r );
+
+                // Render the rect to the screen
+                SDL_RenderPresent(renderer);		
+                
+                // Delay for next frame
+                SDL_Delay(100);
+            //}
             
             //Hack to get window to stay up
             SDL_Event e;
             bool quit = false; 
-            while( quit == false )
-                while( SDL_PollEvent( &e ) )
+            while( quit == false ) 
+               while( SDL_PollEvent( &e ) )
                     if( e.type == SDL_QUIT )
                         quit = true;
 		}
